2c604d77818f7b6c31c23e5d4bbc45f2ea8b67c3
Diego Merello <diegogmerello@gmail.com>
2022-01-25 20:17:12 +0100

    started

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -6,33 +6,15 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import string
+import random
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
-
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
-
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
 n = input('How many random strings to generate? ')
+chars_1 = input('Enter minimum string length: ')
+chars_2= input('Enter maximum string length: ')
+def id_generator (n, chars_1, chars_2):
+    chars_1=string.ascii_lowercase + string.digits
+    chars_2=string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(n))
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(id_generator(n, chars_1, chars_2))
